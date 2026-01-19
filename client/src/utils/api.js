9e5691dd8b86c519be1e84f7d5939055550fa62f
Mfb7 <mehmetfarukbas7@gmail.com>
2026-01-19 21:30:23 +0300

    Update API logic for cloud

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || '/api',
+    baseURL: 'https://servistakip-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
