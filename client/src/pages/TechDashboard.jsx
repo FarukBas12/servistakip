6e7cbb6e5137cdbbbd410a95e1f167fd3b28ecbd
Mfb7 <mehmetfarukbas7@gmail.com>
2026-01-19 22:06:18 +0300

    Update API logic for cloud

--- client/src/pages/TechDashboard.jsx
+++ client/src/pages/TechDashboard.jsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import api from '../utils/api';
+import { Link } from 'react-router-dom';
 import { playNotificationSound } from '../utils/sound';
 
 const TechDashboard = () => {
