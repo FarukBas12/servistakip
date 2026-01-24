0a0598b5a41e8c3134300b15cdc577ad2f54a978
Mfb7 <mehmetfarukbas7@gmail.com>
2026-01-24 22:08:39 +0300

    Bump version to 1.3.8 to force client update

--- server/index.js
+++ server/index.js
@@ -100,7 +100,7 @@ app.use('/api/stock-tracking', require('./routes/stockTracking')); // Distinct f
 
 // Version Endpoint for Auto-Update
 app.get('/api/version', (req, res) => {
-    res.json({ version: '1.3.7' });
+    res.json({ version: '1.3.8' });
 });
 
 // The "catchall" handler: for any request that doesn't
